9b7911d519336864b5774380267f12d7ac1e2f85
RiaRiver <riria.river@gmail.com>
2020-11-04 00:12:24 +0300

    make layout on React

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Features from "./components/Features";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+   <Fragment>
+     <Header />
+     <Main />
+     <Features />
+     <Contact />
+     <Footer />
+   </Fragment>
+  );
+}
+
+export default App;
